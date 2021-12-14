64269fb881958424154c0327da1abe077b25a515
dennisb1239 <50661438+dennisb1239@users.noreply.github.com>
2021-12-14 09:38:29 +0100

    first adaption of base code to my use case

--- cli.py
+++ cli.py
@@ -6,21 +6,23 @@ from mlib import predict
 
 @click.command()
 @click.option(
-    "--weight",
-    prompt="MLB Player Weight",
-    help="Pass in the weight of a MLB player to predict the height",
+    "--OverallQual",
+    prompt="Overall house quality, ranging from 0 to 10",
+    help="Pass in the value for the overall house quality, ranging from 0 to 10",
 )
-def predictcli(weight):
-    """Predicts Height of an MLB player based on weight"""
+def predictcli(OverallQual):
+    """Predicts the sales prive of a house, based on its overall quality"""
 
-    result = predict(weight)
+    result = predict(OverallQual)
+    click.echo(click.style(result, bg = "green", fg = "white")
+    '''
     inches = result["height_inches"]
     human_readable = result["height_human_readable"]
     if int(inches) > 72:
         click.echo(click.style(human_readable, bg="green", fg="white"))
     else:
         click.echo(click.style(human_readable, bg="red", fg="white"))
-
+    '''
 
 if __name__ == "__main__":
     # pylint: disable=no-value-for-parameter
